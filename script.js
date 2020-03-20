6c7181a178cc950434bff4f420dd483a5f5a0cda
vinaykanth-edu <vinaykanth.edu@gmail.com>
2020-03-20 22:34:28 +0530

    sprint-2 project

--- script.js
+++ script.js
@@ -1,62 +1,60 @@
-// function add(inctab){
-
-//     var inc = document.getElementById('inc').value;
-//     var amt = document.getElementById('add').value;
-//     // var exp = document.getElementById('exp').value;
-//     // var sub = document.getElementById('sub').value;
-
-//     var table = document.getElementsById('inctab');
-//     // var newRow = table.insertRow(-1);  
-//     // var cel1 = newRow.insertCell(0);
-//     // var cel2 = newRow.insertCell(1);   
-//     // cel1.innerHTML = inc;
-//     // cel2.innerHTML = amt;
-//     var ele = document.createElement('tr')
-//     var td1 = document.createElement('td')
-//     var td2 = document.createElement('td')
-//     td1.innerHTML = inc
-//     td2.innerHTML = amt
-//     ele.appendChild(td1)
-//     ele.appendChild(td2)  
-//     table.appendChild(ele)
-    
-// }
-// function add(){
-//     var inc = document.getElementById('middle-box1')
-//     var p = document.createElement('p')
-//     p.textContent = document.getElementById('add').value
-//     inc.appendChild(p)
-      
-// }
-var arr = []
-var exp = []
-function add(){   
-    var total = 0
-    
-    var money = document.getElementById('add').value
-    arr.push(money)
-    for(var i=0; i<arr.length; i++)
+var h2 = document.querySelectorAll('h2')
+console.log(h2[0].textContent)
+var btn = document.querySelectorAll('button')
+btn[0].addEventListener('click', incomeUpdate)
+var inp = document.querySelectorAll('input')
+var h1 = document.querySelector('h1')
+var table1 = document.createElement('table')
+btn[1].addEventListener('click', expenseUpdate)
+var table2 = document.createElement('table')
+var display = document.getElementById('display')
+display.append(table1, table2)
+var flag1 = true
+var flag2 = true
+var inc = 0
+var exp = 0
+var i =1
+var j =1
+console.log(h1.textContent)
+function incomeUpdate(){
+    inc = inc + Number(inp[1].value)
+    // console.log(inc)
+    h2[0].textContent = "Income: " + inc
+    h1.textContent = "Available Money: "+ inc
+    // console.log(h1.textContent)
+    var row1 = document.createElement('tr')
+    if(flag1 == true)
     {
-        total = total + Number(arr[i])
+        row1.innerHTML = '<th>'+'Sl.No'+'<th>'+'Income'+'<th>'+'Amount'
+        flag1 = false
     }
-    // var h1 = document.getElementById("h1")
-    // h1.innerHTML = "Available Money: " + money
-    var income = document.getElementById('income')
-    // income.innerHTML = "Income: "+ money
-    // var p = document.createElement('p')
-    income.innerHTML = "Income: "+ total
-   
     
+    var row2 = document.createElement('tr')
+    row2.innerHTML = '<td>'+i+'<td>'+inp[0].value +'<td>'+inp[1].value
+    console.log(row2)
+    i++
+    table1.append(row1, row2)
+    table1.style.float = "left"
+    document.body.append(table1)
 }
-function sub(){
-    var ini_exp = 0
-    var expense =document.getElementById('sub').value
-    var ele2 = document.getElementById('expense')
-    exp.push(expense)
-    for(var j=0; j<exp.length; j++)
+console.log(h2[1].textContent)
+function expenseUpdate(){
+    exp = exp + Number(inp[3].value)
+    console.log(exp)
+    h2[1].textContent = "Expenses: "+ exp
+    h1.textContent = "Available Money: "+ (inc - exp)
+    var row1 = document.createElement('tr')
+    if(flag2 == true)
     {
-        ini_exp = ini_exp + Number(exp[i])
+        row1.innerHTML = '<th>'+'Sl.No'+'<th>'+'Expense'+'<th>'+'Amount'
+        flag2 = false
     }
-    ele2.innerHTML = "Expenses: "+ ini_exp
     
+    var row2 = document.createElement('tr')
+    row2.innerHTML = '<td>'+j+'<td>'+inp[2].value +'<td>'+inp[3].value
+    console.log(row2)
+    j++
+    table2.append(row1, row2)
+    // table2.style.float = "left"
+    document.body.append(table2)
 }
